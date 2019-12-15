a7eb86562b04e104571cf4fde9918ef045159017
alimslimani <slimanialim@yahoo.fr>
2019-12-15 12:30:10 +0100

    alm-005 change default server tomcat to jetty and configure app to deployment

---
+++ conference-demo/jdk11.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Setting JAVA_HOME
+set JAVA_HOME=C:\Program Files\Java\jdk-11.0.5
+echo setting PATH
+set PATH=C:\Program Files\Java\jdk-11.0.5\bin;%PATH%
+echo Display java version
+java -version
+pause;
